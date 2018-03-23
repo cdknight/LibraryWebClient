2cd99715e991ecabfc7d0e7da952cce4cc36ff53
cdknight <enterprisebalaji@hotmail.com>
2018-03-23 14:05:02 -0400

    ItemsOut and Header updated. ItemsOut now working
    Signed-off-by: cdknight <enterprisebalaji@hotmail.com>

--- Assets/Header.php
+++ Assets/Header.php
@@ -18,6 +18,7 @@
             elseif(isset($_SESSION['username'])){
                 echo "<li><a href='/FVLibraryWebClient/UserAccount/Overview.php'>Account Overview</a>";
                 echo "<li><a href='/FVLibraryWebClient/UserAccount/Requests.php'>Requests</a>";
+                echo "<li><a href='/FVLibraryWebClient/UserAccount/ItemsOut.php'>Items Out</a>";
                 echo "<li><a href='/FVLibraryWebClient/UserUtils/Logout.php'>Log Out</a></li>";
                 echo "<li class='nonhover'><a  style='float: right'>Welcome, ".$_SESSION['username']."!</a></li>";
 
--- UserAccount/ItemsOut.php
+++ UserAccount/ItemsOut.php
@@ -1,7 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nonuser
- * Date: 3/23/18
- * Time: 10:20 AM
- */
+session_start();
+if (!isset($_SESSION['username'])) {
+    //user not logged in
+    $_SESSION['msg'] = "<p style='color:red'>You must log in first in order to view your checked-out items</p>";
+    header("Location: ../Login.php");
+}
+include('../Assets/Header.php');
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Items Out</title>
+    <link rel="stylesheet" type="text/css" href="../main.css">
+</head>
+<body>
+<h1 class="title">Items Out</h1>
+<?php
+    $conn = new mysqli('localhost', 'default_u', 'letmeinmysql', 'lcatalog');
+    //echo "userid: " + getUserIdFromName($conn, $_SESSION['username']);
+    $query = "SELECT * FROM ItemsOut WHERE userid=".getUserIdFromName($conn, $_SESSION['username']);
+    //echo "<br>".$query;
+    $result = $conn->query($query);
+
+    while ($row = $result->fetch_assoc()){
+        echo "<a href='../BookPage.php?id=".$row['bookid']."'>".getBookTitleFromId($conn, $row['bookid'])."</a><br>";
+    }
+
+
+    function getUserIdFromName($conn, $name){
+        $result = $conn->query("SELECT id FROM Users WHERE username=\"".$name."\"");
+        while ($row = $result->fetch_assoc()){
+            return $row['id'];
+        }
+    }
+    function getBookTitleFromId($conn, $id){
+        $result = $conn->query("SELECT Title FROM Books WHERE ID=".$id);
+        while ($row = $result->fetch_assoc()){
+            return $row['Title'];
+        }
+    }
+?>
+
+</body>
+</html>
