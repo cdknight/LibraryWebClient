d2a689b37d035b977267548d0c707a719dbf23e0
Ramesh Balaji <post2rb@hotmail.com>
2018-05-28 21:32:07 -0400

    working functionality for recent books
    Signed-off-by: Ramesh Balaji <post2rb@hotmail.com>

--- index.php
+++ index.php
@@ -17,12 +17,16 @@ include("Assets/Header.php");
         <h1 class="title">Fellowship Village Library Catalog</h1>
         <p class="title">Welcome to the Fellowship Village Library Catalog. Press "Login" if you want to login, and "Search" if you want to search. <br>If you would like to create an account for the catalog, please contact the librarian.
         </p>
+        <h3 class="title">Recent Items: </h3>
         <?php
         $conn = new mysqli('localhost', 'default_u', 'letmeinmysql', 'lcatalog');
         //echo "rbooklist: ". $_SESSION['recent_book_list'].count($_SESSION['recent_book_list']);
         //print_r($_SESSION['recent_book_list']);
         foreach ($_SESSION['recent_book_list'] as $key => $value){
-            echo "<a href='BookPage.php?id='".$value."'><img height=\"10%\" width=\"10%\" src='Assets/book.png'>".getBookTitleFromId($conn, $key)."</a>";
+            if (!$key == 0){
+                echo "<div class='carousel'><div class='recent_book'><img height=\"10%\" width=\"10%\" src='Assets/book.png'><br><a href='BookPage.php?id='".$value."'>".getBookTitleFromId($conn, $key)."</a></div></div>";
+            }
+
         }
         function getBookTitleFromId($conn, $id){
             $result = $conn->query("SELECT Title FROM Books WHERE ID=".$id);
