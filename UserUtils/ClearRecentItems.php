416c9a8ab05a1ef66dc9ffe535065ab905498a8b
Ramesh Balaji <post2rb@hotmail.com>
2018-05-28 22:25:03 -0400

    touchups
    Signed-off-by: Ramesh Balaji <post2rb@hotmail.com>

--- UserUtils/ClearRecentItems.php
+++ UserUtils/ClearRecentItems.php
@@ -1,9 +1,14 @@
+
 <?php
 session_start();
 $clear_bool = $_REQUEST['doclear'];
+echo "<div class='uifixes'>";
 if ($clear_bool){
     unset($_SESSION['recent_book_list']);
     echo "<p style='color:green'>Cleared list successfully!</p>";
 }
-echo "<p style='color:red'>Unable to clear list!</p>"
+else {
+    echo "<p style='color:red'>Unable to clear list!</p>";
+}
+echo "</div>"
 ?>
