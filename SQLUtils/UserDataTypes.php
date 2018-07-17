ca8aaf32bdfd23da9472b675bdb186590c93ea8a
Ramesh Balaji <post2rb@hotmail.com>
2018-07-17 00:00:33 -0400

    work on getUserData.php
    Signed-off-by: Ramesh Balaji <post2rb@hotmail.com>

---
+++ SQLUtils/UserDataTypes.php
@@ -0,0 +1,11 @@
+<?php
+abstract class UserDataTypes
+{
+    const ID = 'id';
+    const FIRSTNAME = 'firstname';
+    const LASTNAME = 'lastname';
+    const H_PASSWORD = 'password';
+    const USERNAME = 'username';
+    const EMAIL = 'email';
+    const APT = 'apt';
+}
