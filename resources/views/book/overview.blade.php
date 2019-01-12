f1580615d10ce1f264a8981a6e5f84ca8fcb5761
Ramesh Balaji <post2rb@hotmail.com>
2019-01-11 21:55:33 -0500

    Update indentation on book overview file

--- resources/views/book/overview.blade.php
+++ resources/views/book/overview.blade.php
@@ -16,61 +16,63 @@
       <p class="lead"><b>Genre:</b> {{ $book->Genre }}</p>
 
       @if ( $book->CheckedOut )
-      <p class="lead"><b>Note: </b>This item is checked out.</b>
-@endif
+	  <p class="lead"><b>Note: </b>This item is checked out.</b>
+      @endif
 
-   @if ( $book->Missing )
-       <p class="lead"><b>Note: </b>This item is missing.</p>
-   @endif
+      @if ( $book->Missing )
+	  <p class="lead"><b>Note: </b>This item is missing.</p>
+      @endif
 
 
-   @if ( isset($book->bookInformation) )
+      @if ( isset($book->bookInformation) )
 
-       <h3>Book Information</h3>
+	  <h3>Book Information</h3>
        
-       <ul class="list-group">
+	  <ul class="list-group">
 
-	 {{--- In the following, we check if the corresponding properties are set for a book and display them if they are. Not all books have a full entry in the database. ---}}
-	 @if ( isset($book->bookInformation->description) )
-	    <li class="list-group-item">
-              <b>Description</b>: {{ $book->bookInformation->description }}
-	    </li>
-	 @endif
+	      {{--- In the following, we check if the corresponding properties are set for a book and display them if they are. Not all books have a full entry in the database. ---}}
+	      @if ( isset($book->bookInformation->description) )
+		  <li class="list-group-item">
+		      <b>Description</b>: {{ $book->bookInformation->description }}
+		  </li>
+	      @endif
 
-         @if ( isset($book->bookInformation->publisher) )
-	    <li class="list-group-item">
-	      <b>Publisher</b>: {{ $book->bookInformation->publisher}}
-	    </li>
-	 @endif
+              @if ( isset($book->bookInformation->publisher) )
+		  <li class="list-group-item">
+		      <b>Publisher</b>: {{ $book->bookInformation->publisher}}
+		  </li>
+	      @endif
 
-         @if ( isset($book->bookInformation->isbn) )
-	    <li class="list-group-item">
-              <b>ISBN Number</b>: {{ $book->bookInformation->isbn}}
-	    </li>
-	 @endif
+              @if ( isset($book->bookInformation->isbn) )
+		  <li class="list-group-item">
+		      <b>ISBN Number</b>: {{ $book->bookInformation->isbn}}
+		  </li>
+	      @endif
 
-         @if ( isset($book->bookInformation->publication_date) )
-	    <li class="list-group-item">
-              <b>Publication Date</b>: {{ $book->bookInformation->publication_date}}
-	    </li>
-	 @endif
+              @if ( isset($book->bookInformation->publication_date) )
+		  <li class="list-group-item">
+		      <b>Publication Date</b>: {{ $book->bookInformation->publication_date}}
+		  </li>
+	      @endif
 
-       </ul>
+	  </ul>
        
-   @endif
+      @endif
 
-</div>
+    </div>
 
-<div class="float-right">
-  <ul class="list-group">
-    <li class="list-group-item"><h2>Actions</h2></li>
-    <li class="list-group-item"><button class="btn btn-success">Place Hold</btn></li>
-    <li class="list-group-item"><button class="btn btn-warning">Contribute Feedback</btn></li>
-  </ul>
-</div>
-  {{--- Actions list group ---}}
+    {{--- Actions list group ---}}
 
-</div>
+    <div class="float-right">
+	<ul class="list-group">
+	    <li class="list-group-item"><h2>Actions</h2></li>
+	    <li class="list-group-item"><button class="btn btn-success">Place Hold</btn></li>
+		<li class="list-group-item"><button class="btn btn-warning">Contribute Feedback</btn></li>
+	</ul>
+    </div>
+
+
+  </div>
        
 </div>
     
